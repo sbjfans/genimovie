35aa0388dacb4a9f092066b2d92bb414e10ab052
sbjstar <sonbongju7@gmail.com>
2024-08-08 21:19:31 +0900

    또다시

--- models.py
+++ models.py
@@ -148,6 +148,7 @@ class Influencer(db.Model):
     influencer_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     bio = db.Column(db.Text, nullable=True)
+    img_nm = db.Column(db.String(100), nullable=False)
     created_by = db.Column(db.Integer, nullable=False)
     updated_by = db.Column(db.Integer, nullable=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
